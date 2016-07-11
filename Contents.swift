671dd5885a698fb0a4e8319a9cb0811ad6a295a4
plogue <prc-lh138-06@PRC-LH138-06s-iMac.local>
2016-07-11 15:51:15 -0500

    cal

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number = 0
+var number1 = 10
+
+var number2 = 2
+
+let addition = number1 + number2
+let subtraction = number1 - number2
+let division = number1 / number2
+let multiple = number1 * number2
+
+print(addition, terminator:"")
+print(subtraction, terminator:"")
+print(division, terminator:"")
+print(multiple, terminator:"")
+
+
